2b8052238aaa0910e314613ce2a35c45403ef724
Gary Wu <s19003045@gmail.com>
2020-01-14 22:38:50 +0800

    新增課程種子〈CS50-mobile app development〉

--- true_seeds_gary/courses.js
+++ true_seeds_gary/courses.js
@@ -50,6 +50,32 @@ const courses = [
     UserId: 1, // 固定
     createdAt: new Date(), // 固定
     updatedAt: new Date() // 固定
+  },
+  {
+    name: "CS50's Mobile App Development with React Native", // 課程名稱
+    // 課程簡介
+    description:
+      "This course picks up where Harvard College’s CS50 leaves off, transitioning from web development to mobile app development with React Native, a popular open-source framework maintained by Facebook that enables cross-platform native apps using Javas..",
+    image: "hhttps://i.imgur.com/7gvksse.png", // 上傳到IMGUR再複製連結
+    introVideo: "https://www.youtube.com/embed/_P7wHN_kOv4", // youtube嶔入
+    teacherName: "David J. Malan", // 老師名字
+    // 老師介紹
+    teacherDescrip:
+      "David J. Malan (/meɪlɛn/) is Gordon McKay Professor of the Practice of Computer Science at Harvard University.[1][2] Malan is best known for teaching Computer Science 50 (known as CS50)[3][4][5][6] which is the largest course at Harvard and the ....",
+    totalTime: 586, //單位(minutes) //依照影片長度
+    totalLessons: 6, // 依照lessons.js裡面該課程的lesson數量
+    price: 1000, // 1000~5000 自訂
+    status: "intoMarket", // 固定
+    submittedDate: new Date(), // 固定
+    intoMarketDate: new Date(), // 固定
+    ratingAverage: 5, // 1~5 integer
+    ratingCount: 5, // 1~6 integer
+    studentCount: 0, // 預設為0，以保持學生人數計算正確
+    CourseCategoryId: 1, // 依照類別種子資料輸入，參考seeders裡面Category-and-SubCategory
+    CourseSubCategoryId: 51, // 依照類別種子資料輸入，參考seeders裡面Category-and-SubCategory
+    UserId: 1, // 固定
+    createdAt: new Date(), // 固定
+    updatedAt: new Date() // 固定
   }
 ];
 
